f7a1c280c5300a4252afa9f6ba1e3eecf0e079b2
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-11-20 19:34:14 -0500

    Evidencia 5

---
+++ Evidencia 5/main.js
@@ -0,0 +1,23 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    let form=document.querySelector('#input')
+    let cont =1   
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();       
+        if(cont <=50){
+        let datosEntrada = Object.fromEntries(new FormData(e.target))
+            
+            let observaciones= datosEntrada.observaciones
+            let hora= (datosEntrada.hora)
+        
+            let tabla=document.querySelector("tbody");
+            tabla.insertAdjacentHTML("beforeend",`
+                <tr>
+                    <td> ${cont}  </td>
+                    <td> ${observaciones}  </td>
+                    <td> ${hora} </td>
+                </tr>
+                `)
+            cont++
+        }
+    })               
+})
